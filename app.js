9fe87c4398e3e16b440f9e336650743fd298c9b3
Ollie Relph <ollie@relph.me>
2010-11-14 23:24:47 +0000

    Got some basic user administration going

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+var imgarr = [{ "_id" : "8e0c104cb62b134d25020000", "userid" : "e12d364c859e098f01010000", "linkid" : "9Bi2T", "sizes" : { "o" : { "width" : 717, "height" : 691, "name" : "7rny8Swv1luRregPnUTu1kukcHEbW6oELwiKbrUz_o.png", "views" : 22 }, "s" : { "width" : 156, "height" : 150, "name" : "7rny8Swv1luRregPnUTu1kukcHEbW6oELwiKbrUz_s.png", "views" : 745 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 22:50:06 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "f40f104c586a114d25040000", "userid" : "e12d364c859e098f01010000", "linkid" : "6XT2w", "sizes" : { "o" : { "width" : 904, "height" : 604, "name" : "NTge8XLwn9qobm4z7tlIO5qbI8laHOydGuH4UPlE_o.png", "views" : 19 }, "s" : { "width" : 200, "height" : 134, "name" : "NTge8XLwn9qobm4z7tlIO5qbI8laHOydGuH4UPlE_s.png", "views" : 739 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:04:36 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "1310104c5ddf084d25070000", "userid" : "e12d364c859e098f01010000", "linkid" : "Da0t0", "sizes" : { "o" : { "width" : 401, "height" : 474, "name" : "bol0NeZwOrvTzDwzu0TdEX0tzGSIvpPlImlXFChC_o.png", "views" : 3 }, "s" : { "width" : 127, "height" : 150, "name" : "bol0NeZwOrvTzDwzu0TdEX0tzGSIvpPlImlXFChC_s.png", "views" : 728 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:05:07 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "0110104c8d1b0a4d25050000", "userid" : "e12d364c859e098f01010000", "linkid" : "RuTsD", "sizes" : { "o" : { "width" : 506, "height" : 677, "name" : "HVKd7h4IsFttCdVT4avXIDgPSWelzT6xmMQJbMQr_o.png", "views" : 3 }, "s" : { "width" : 112, "height" : 150, "name" : "HVKd7h4IsFttCdVT4avXIDgPSWelzT6xmMQJbMQr_s.png", "views" : 728 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:04:49 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "0c10104c96a7014d25060000", "userid" : "e12d364c859e098f01010000", "linkid" : "AP5tQ", "sizes" : { "o" : { "width" : 459, "height" : 586, "name" : "2mZtdlRSyD0y91o1z55cffkvhD2DaSnUTdPnaevm_o.png", "views" : 2 }, "s" : { "width" : 117, "height" : 150, "name" : "2mZtdlRSyD0y91o1z55cffkvhD2DaSnUTdPnaevm_s.png", "views" : 728 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:05:00 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "3f17104c45660c4d25090000", "userid" : "e12d364c859e098f01010000", "linkid" : "mbEdU", "sizes" : { "o" : { "width" : 496, "height" : 575, "name" : "EVu8TDRAxaw6PLd96oKBilUgacdV52EOIH8DrTEJ_o.png", "views" : 6 }, "s" : { "width" : 129, "height" : 150, "name" : "EVu8TDRAxaw6PLd96oKBilUgacdV52EOIH8DrTEJ_s.png", "views" : 716 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:35:43 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "4b18104cf768024d250a0000", "userid" : "e12d364c859e098f01010000", "linkid" : "mgf5F", "sizes" : { "o" : { "width" : 472, "height" : 319, "name" : "V5vCMu4U0dZOsTuu1VKW9L9Uhx2vdOL2cBoryJdU_o.png", "views" : 6 }, "s" : { "width" : 200, "height" : 135, "name" : "V5vCMu4U0dZOsTuu1VKW9L9Uhx2vdOL2cBoryJdU_s.png", "views" : 715 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:40:10 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "5f18104c36e0114d250c0000", "userid" : "e12d364c859e098f01010000", "linkid" : "Qpkf4", "sizes" : { "o" : { "width" : 359, "height" : 364, "name" : "myayF8LiQDDDrkiIAEzgv31RHfLoWH89WWKrQJbV_o.png", "views" : 1 }, "s" : { "width" : 148, "height" : 150, "name" : "myayF8LiQDDDrkiIAEzgv31RHfLoWH89WWKrQJbV_s.png", "views" : 716 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:40:31 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "9e18104c5fd4114d250f0000", "userid" : "e12d364c859e098f01010000", "linkid" : "td0bs", "sizes" : { "o" : { "width" : 360, "height" : 477, "name" : "9S4h06SLUTZqwTEpHFn4L501UgvTVkVWev1GB5g5_o.png", "views" : 5 }, "s" : { "width" : 113, "height" : 150, "name" : "9S4h06SLUTZqwTEpHFn4L501UgvTVkVWev1GB5g5_s.png", "views" : 715 }, "custom" : null }, "uploaded" : "Wed Jun 09 2010 23:41:34 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "48fe114ceae5093243010000", "userid" : "e12d364c859e098f01010000", "linkid" : "180qi", "sizes" : { "o" : { "width" : 280, "height" : 419, "name" : "8CCx2egdLG6WETh2iMJRhpCAUXB49fnn00VoqbuJ_o.png", "views" : 3 }, "s" : { "width" : 100, "height" : 150, "name" : "8CCx2egdLG6WETh2iMJRhpCAUXB49fnn00VoqbuJ_s.png", "views" : 710 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:13:44 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "a1fe114c5e73063243030000", "userid" : "e12d364c859e098f01010000", "linkid" : "pIeLu", "sizes" : { "o" : { "width" : 596, "height" : 766, "name" : "7W4fm6OodzNgrhoFihUeN9HJmkNEfl0w6pHeRSUb_o.png", "views" : 2 }, "s" : { "width" : 117, "height" : 150, "name" : "7W4fm6OodzNgrhoFihUeN9HJmkNEfl0w6pHeRSUb_s.png", "views" : 710 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:15:13 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "bbfe114c6917123243050000", "userid" : "e12d364c859e098f01010000", "linkid" : "h1xLQ", "sizes" : { "o" : { "width" : 825, "height" : 638, "name" : "ax3q9l9uMILpKLZsaAOcvk7QV651k1CgZkx5VRtt_o.png", "views" : 7 }, "s" : { "width" : 194, "height" : 150, "name" : "ax3q9l9uMILpKLZsaAOcvk7QV651k1CgZkx5VRtt_s.png", "views" : 716 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:15:39 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "c7fe114cd32a043243060000", "userid" : "e12d364c859e098f01010000", "linkid" : "QV3RQ", "sizes" : { "o" : { "width" : 1022, "height" : 684, "name" : "S0St89ttU0qMMZWMViUr8cUzs6y0ZUXlyDd9NvAn_o.png", "views" : 4 }, "s" : { "width" : 200, "height" : 134, "name" : "S0St89ttU0qMMZWMViUr8cUzs6y0ZUXlyDd9NvAn_s.png", "views" : 710 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:15:51 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "fcfe114c8324113243070000", "userid" : "e12d364c859e098f01010000", "linkid" : "U84Vx", "sizes" : { "o" : { "width" : 610, "height" : 781, "name" : "cThMbTnDd3Pz01CDNmbOK2gBHihHGdPWtKP2vhRU_o.png", "views" : 2 }, "s" : { "width" : 117, "height" : 150, "name" : "cThMbTnDd3Pz01CDNmbOK2gBHihHGdPWtKP2vhRU_s.png", "views" : 710 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:16:44 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "07ff114c7b6b083243080000", "userid" : "e12d364c859e098f01010000", "linkid" : "do5A1", "sizes" : { "o" : { "width" : 639, "height" : 783, "name" : "7xp02Vv9yfLTsFSLCWvAb02NpAqc6F1lnAxeM8gk_o.png", "views" : 1 }, "s" : { "width" : 122, "height" : 150, "name" : "7xp02Vv9yfLTsFSLCWvAb02NpAqc6F1lnAxeM8gk_s.png", "views" : 710 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:16:55 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "18ff114cd93c0432430a0000", "userid" : "e12d364c859e098f01010000", "linkid" : "2KDpg", "sizes" : { "o" : { "width" : 406, "height" : 563, "name" : "u6eAU6FgZNccBw4okKtDlEluRZaCymZ2RgGBGRPx_o.png", "views" : 2 }, "s" : { "width" : 108, "height" : 150, "name" : "u6eAU6FgZNccBw4okKtDlEluRZaCymZ2RgGBGRPx_s.png", "views" : 710 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:17:12 GMT+0100 (BST)", "passcode" : "" },{ "_id" : "38ff114cdc0f1132430c0000", "userid" : "e12d364c859e098f01010000", "linkid" : "U7MB3", "sizes" : { "o" : { "width" : 599, "height" : 849, "name" : "5rdxnBOJSbLbTemCwMRtbdQuJD2InqiGAhdhlTg4_o.png", "views" : 0 }, "s" : { "width" : 106, "height" : 150, "name" : "5rdxnBOJSbLbTemCwMRtbdQuJD2InqiGAhdhlTg4_s.png", "views" : 710 }, "custom" : null }, "uploaded" : "Fri Jun 11 2010 10:17:44 GMT+0100 (BST)", "passcode" : "" }];
+
 /**
  * Module dependencies.
  */
- 
-var mongoose = require('./vendor/mongoose/lib/mongoose'), 
-	document = mongoose.define;
- 
-var db = mongoose.connect('mongodb://localhost/mongoose');
 
 var express = require('express'),
     sys = require('sys');
 
+ 
+var mongoose = require('./vendor/mongoose/lib/mongoose'), 
+	document = mongoose.define;
+ 
+var db = mongoose.connect('mongodb://localhost/screwup');
 var app = module.exports = express.createServer(
-		express.logger(),
 	    express.cookieDecoder(),
 	    express.session()
     );
@@ -20,21 +21,66 @@ var app = module.exports = express.createServer(
 // Configuration
 
 app.configure(function () {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyDecoder());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyDecoder());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.staticProvider(__dirname + '/public'));
 });
 
 app.configure('development', function () {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function () {
-  app.use(express.errorHandler()); 
+	app.use(express.errorHandler()); 
 });
+
+// Models
+document('User')
+	.oid('_id')
+	.string('username')
+	.string('password')
+	.string('email')
+	.date('joined')
+	.date('lastlogin')
+	.string('role')
+	.boolean('deleted');
+	
+var User = mongoose.User;
+	
+document('Image')
+	.oid('_id')
+	.object('userid')
+	.string('linkid')
+	.object('sizes', 
+		document()
+			.object('o', 
+				document()
+					.number('width')
+					.number('height')
+					.string('name')
+					.number('views')
+			)
+		.object('s',
+			document()
+				.number('width')
+				.number('height')
+				.string('name')
+				.number('views')
+		)
+	)
+	.date('uploaded')
+	.date('edited')
+	.string('passcode')
+	.boolean('deleted')
+	
+
+var Image = mongoose.Image;
+
+
+
 
 // Errors
 
@@ -89,7 +135,7 @@ app.error(function(err, req, res, next){
 });
 
 // User Auth
-var User = {
+var UserAuth = {
 	Login : function (req) {
 		req.session.user = {};
 		req.session.user.loggedIn = true;
@@ -113,12 +159,35 @@ var User = {
  * /a/ -> admin
  * /e/ -> error
  */
+ 
+ 
+ // Wildcards
+ app.get('/css/:file', function(req, res) {	
+ 	var file = req.params.file; 
+ 	console.log('proxying: ' + './public/css/' + file);
+ 	res.sendfile('./public/css/' + file);
+ });
+ app.get('/js/:file', function(req, res) {	
+ 	var file = req.params.file; 
+ 	console.log('proxying: ' + './public/js/' + file);
+ 	res.sendfile('./public/js/' +  file);
+ });
+ app.get('/img/:file', function(req, res) {	
+ 	var file = req.params.file; 
+ 	console.log('proxying: ' + './public/img/' + file);
+ 	res.sendfile('./public/img/' +  file);
+ });
 
 app.get('/', function(req, res) {
 
-	console.log('User is logged in: ' + (User.IsLoggedIn(req) ? 'true' : 'false'));
+
 	
-	if (User.IsLoggedIn(req)) {
+	
+	
+
+	console.log('User is logged in: ' + (UserAuth.IsLoggedIn(req) ? 'true' : 'false'));
+	
+	if (UserAuth.IsLoggedIn(req)) {
 	
 		res.redirect('/l/all');
 		
@@ -137,68 +206,210 @@ app.get('/', function(req, res) {
  
 app.get('/l/all', function (req, res) {
 	
-	if (!User.IsLoggedIn(req)) {
+	if ( 1 != 1) {//!UserAuth.IsLoggedIn(req)) {
 		res.redirect('/u/login');
-	} 	
-	res.render('image-list', {
-	  locals: {
-	    title: 'All',
-	    images: []
-	  }
-	});	
-	/*
-	
-	Img.find({ userid: self.session.user._id  }).sort('uploaded', 'desc').each(function(img) {
-	
-		this.partial(img);
-		
-	}).then(function(imgs){
-		self.render('list.jade', {
+	} else {
+//		Image.find('sizes.o.views', '1').first(function(err, img){
+//			console.log('Image: ' + img);
+//		});
+		Image.all(function(err, docs){
+		  
+		  console.log("Error: " + err);  
+		  console.log("Image: " + docs.length);  
+		  
+		  
+		  
+		  res.render('images', {
 		    locals: {
-		      	title: 'List: All Images',
-				isAuthd: self.session.isAuthd,
-				debugmode: config.app.debugmode,
-				currentpath: self.url.pathname,
-				images: imgs,
-				basepath:  config.images.basePath
+		      title: 'All',
+		      images: imgarr
 		    }
+		  });	
+		  
 		});
-	});
-	*/
-
+		
+		/*
+		
+		Img.find({ userid: self.session.user._id  }).sort('uploaded', 'desc').each(function(img) {
+		
+			this.partial(img);
+			
+		}).then(function(imgs){
+			self.render('list.jade', {
+			    locals: {
+			      	title: 'List: All Images',
+					isAuthd: self.session.isAuthd,
+					debugmode: config.app.debugmode,
+					currentpath: self.url.pathname,
+					images: imgs,
+					basepath:  config.images.basePath
+			    }
+			});
+		});
+		*/
+	}
 			
 });
  
  /*
-  * /u/ is for User
+  * /i/ is for Image
   */
-app.post('/u/create', function () {      
-	var self = this,
-		user = self.params.post.username,
-		email = self.params.post.email,
-		pass = self.params.post.password;		
-			
-	Usr.find({ username : user }).one(function(usr) {
-		return false
-    }, true);
-	
-	Usr.find({ email : email }).one(function(usr) {
-		return false
-    }, true);	
-	
-	// create a new db entry
-	var usr = new Usr({
-		username : user,
-		password : pass,
-		email : email,
-		joined : new Date(),
-		lastlogin : new Date(),
-		role : 'NORMAL'
-	});
-	// save it
-	usr.save();
+  app.post('/i/create', function (req, res) {      
+  
+  
+  	var image = new Image({ 
+  		"userid" : "e12d364c859e098f01010000", 
+  		"linkid" : "k2fcK", 
+  		"sizes" : { 
+  			"o" : { 
+  				"width" : 1159, 
+  				"height" : 933, 
+  				"name" : "6mpxypbXeAdzisMT5wIpFNso0ATvdeyELCf2gSrl_o.png", 
+  				"views" : 7 
+  			}, 
+  			"s" : { 
+  				"width" : 186, 
+  				"height" : 150, 
+  				"name" : "6mpxypbXeAdzisMT5wIpFNso0ATvdeyELCf2gSrl_s.png", 
+  				"views" : 710 
+  			}, 
+  			"custom" : null 
+  		}, 
+  		"uploaded" : "Fri Jun 11 2010 10:20:00 GMT+0100 (BST)", 
+  		"passcode" : "" 
+  	});
+  	
+  	
+  	image.save(function(err){
+  		console.log(err);
+  		
+  	})
+});
+   
+/*
+ * /u/ is for User 
+ */
+app.get('/u/edit/:username', function (req, res) {   
 
-	return true;
+	console.log('Editing user: ' + req.params.username);  
+	
+	if (typeof req.params.username != 'undefined') {
+	
+		User.find({ username : req.params.username }).one(function(err, doc) {
+		
+			if (typeof doc != 'undefined') {
+						      	
+				res.render('edit-user', {
+				  locals: {
+				    title: 'Edit User',
+				    user: doc
+				  }
+				});	
+				
+			} else {
+			
+				//TODO: Raise error
+				console.log('No user with username: ' + req.params.username);
+			}
+			
+			
+			
+		});
+	}
+});
+
+app.post('/u/edit/:username', function (req, res) { 
+ 
+	console.log('Saving user: ' + req.params.username);  
+	
+	if (typeof req.params.username != 'undefined') { 
+	
+		if (!req.body || !req.body.user || !req.body.user.name || !req.body.user.password || !req.body.user.email) {
+		
+			console.log('HAXX');
+		
+		} else {
+			
+			// TODO: Some big ole security holes to be filled
+			User.find({ username : req.params.username }).one(function(err, doc) {
+			
+				console.log(err, doc);
+			
+				if (typeof doc  != "undefined") {
+				
+					doc.username = req.body.user.name;
+					doc.password = req.body.user.password;
+					doc.email = req.body.user.email;
+					
+					doc.save(function (err) {
+						if (err) { 
+							console.log('Error saving user: ' + user + ' - ' + err);
+						}
+						
+						res.redirect('/a/');
+					});
+					
+					
+				} else {
+					//TODO: Raise error
+					console.log('No user with username: ' + req.params.username);				
+				
+					
+					res.redirect('/a/');
+				
+				}
+				
+				
+			});
+		
+		}
+		
+	}	
+});
+app.post('/u/create', function (req, res) {      
+
+	console.log('lets save this biatch: ' + req.body.user );
+
+	if (!req.body || !req.body.user || !req.body.user.name || !req.body.user.password || !req.body.user.email) {
+	
+		console.log('HAXX');
+		res.redirect('back');
+		
+	} else {
+		
+		User.find({ username : req.body.user.name }).one(function(err, doc) {
+		
+			console.log(err, doc);
+		
+			if (typeof doc  == "undefined") {
+			
+				// create a new db entry
+				var user = new User({
+					username : req.body.user.name,
+					password : req.body.user.password,
+					email : req.body.user.email,
+					joined : new Date(),
+					lastlogin : new Date(),
+					role : 'NORMAL'
+				});
+				
+				user.save(function (err) {
+					if (err) { 
+						console.log('Error saving user: ' + user + ' - ' + err);
+					}
+					
+					res.redirect('back');
+				});
+				
+			} else {
+			
+				// TODO: throw error
+				console.log('user already exists');
+				res.redirect('back');
+			}
+		}, true);
+	
+	}
 });
 
 app.post('/u/login', function (req, res) {     	  
@@ -220,7 +431,7 @@ app.post('/u/login', function (req, res) {
 			// check if user is in db... 
 			
 			// set session cookie
-			User.Login(req);
+			UserAuth.Login(req);
 			// 
 			res.redirect('/');
 			/*
@@ -256,9 +467,25 @@ app.get('/u/logout', function () {
 	
 });
 
-// Only listen on $ node app.js
+/*
+ * /a/ is for Admin
+ */
+ 
+app.get('/a/', function (req, res) {
+	
+	User.all(function(err, docs) {
+		console.log(docs);
+		res.render('admin', {
+		  locals: {
+		    title: 'Admin',
+		    users: docs
+		  }
+		});	
+	});
+});
 
+// Only listen on $ node app.js
 if (!module.parent) {
   app.listen(3000);
-  console.log("Express server listening on port %d", app.address().port)
+  console.log("Express server listening on port %d", app.address().port);
 }
--- readme.md
+++ readme.md
@@ -4,19 +4,4 @@ Image Handler & manager, designed for use with [scrup](http://github.com/rsms/sc
 
 _Very fragile, still in development_
 
-## TODO
- * Style
- * Stats for
- * Logging (denied attempts to access a private image etc.)  
- * AJAX-y page load
- * Basic analytics
- * Keyboard Nav... arrows to move... space to zoom... alt to show shortcuts
- * Image list sorting
- * Pagination - loading more on scroll
- * Websockets & true pushing of new images
- * Groups (+ Passwords) ?
- * Tagging ?
- * Abstract DB Management
- * CRUD through web interface
- * API Auth && CRUD through WS
- 
+*Not even remotely ready for production*
