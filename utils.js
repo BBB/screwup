9a7d262238c7f0a42820e45c7354aeb4bfdfd763
Ollie Relph <ollie@relph.me>
2010-11-14 17:54:56 +0000

    moving utils.js out of vendor/

---
+++ utils.js
@@ -0,0 +1,9 @@
+exports.randomstring = function (len) {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var randomstring = '';
+	for (var i = 0; i < len; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	return randomstring;
+};
